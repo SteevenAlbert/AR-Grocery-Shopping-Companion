bf3db7dc8130b1c880dccc968be0e5d446bed159
laila-elhattab <liko.hani@gmail.com>
2022-10-27 22:49:06 +0200

    products listed (Home Page)

---
+++ lib/models/product.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String? image, title, description;
+  final int? price, id;
+  Product({
+    this.id,
+    this.image,
+    this.title,
+    this.price,
+    this.description,
+  });
+}
+
+List<Product> products = [
+  Product(
+      id: 1,
+      title: "     Greek Yogurt",
+      price: 30,
+      description: "Juhayna",
+      image: "images/berrries_1.png"),
+  Product(
+      id: 2,
+      title: "     Skimmed Milk",
+      price: 50,
+      description: "Juhayna",
+      image: "images/milk-2.png"),
+];
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ar_grocery_companion/components/categories_bar.dart';
 import 'package:ar_grocery_companion/screens/home/components/header.dart';
+import 'package:ar_grocery_companion/models/product.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,6 +16,48 @@ class HomePage extends StatelessWidget {
         children: <Widget>[
           Header(size: size),
           const CategoriesBar(),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              height: 180,
+              width: 160,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 226, 210, 209),
+                  borderRadius: BorderRadius.circular(16)),
+              child: Image.asset(products[0].image!),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20 / 4),
+            child: Text(products[0].title!,
+                style: TextStyle(color: Color.fromARGB(255, 101, 100, 100))),
+          ),
+          const Text(
+            "     \£\E 234",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              height: 180,
+              width: 160,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 226, 210, 209),
+                  borderRadius: BorderRadius.circular(16)),
+              child: Image.asset(products[1].image!),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20 / 4),
+            child: Text(products[1].title!,
+                style: TextStyle(color: Color.fromARGB(255, 101, 100, 100))),
+          ),
+          const Text(
+            "     \£\E 234",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )
         ],
       ),
     );
