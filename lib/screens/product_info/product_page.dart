1dcccf5057ad9ae827cc9b84322983ace40df9b6
judykhairalla <judykhairalla@gmail.com>
2022-12-24 21:59:07 +0200

    Colors unified

--- lib/screens/product_info/product_page.dart
+++ lib/screens/product_info/product_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:like_button/like_button.dart';
