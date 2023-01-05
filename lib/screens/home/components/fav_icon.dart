e25f704e03cd9257fedab0ed9688ef0433647e28
laila-elhattab <liko.hani@gmail.com>
2023-01-05 20:43:42 +0200

    fav page state management

--- lib/screens/home/components/fav_icon.dart
+++ lib/screens/home/components/fav_icon.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:favorite_button/favorite_button.dart';
 import 'package:ar_grocery_companion/models/user/customer.dart';
 import 'package:ar_grocery_companion/models/product.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavIcon extends ConsumerWidget {
+class FavIcon extends StatefulWidget {
   const FavIcon({super.key, required this.product});
   final Product product;
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    List<int> favs = ref.watch(favsProvider);
+  State<FavIcon> createState() => _FavIconState();
+}
+
+class _FavIconState extends State<FavIcon> {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      width: 30,
-      height: 30,
+      width: 41,
+      height: 41,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         border: Border.all(
@@ -21,18 +25,40 @@ class FavIcon extends ConsumerWidget {
           style: BorderStyle.solid,
         ),
       ),
-      child: FavoriteButton(
-        iconSize: 30,
-        isFavorite: false,
-        iconColor: Colors.red,
-        iconDisabledColor: Colors.amber.withOpacity(0.4),
-        valueChanged: (isFavorite) {
-          if (isFavorite) {
-            ref.read(favsProvider.notifier).addItem(product.id!);
-          } else {
-            ref.read(favsProvider.notifier).removeItem(product.id!);
-          }
-        },
+      child: Center(
+        child: Consumer(
+          builder: (_, WidgetRef ref, __) {
+            ref.watch(favsProvider);
+            bool isFavo =
+                ref.watch(favsProvider.notifier).isFav(widget.product.id!);
+            return Column(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      if (isFavo) {
+                        isFavo = false;
+                      } else {
+                        isFavo = true;
+                      }
+                      if (isFavo) {
+                        ref
+                            .read(favsProvider.notifier)
+                            .addItem(widget.product.id!);
+                      } else {
+                        ref
+                            .read(favsProvider.notifier)
+                            .removeItem(widget.product.id!);
+                      }
+                    },
+                    icon: Icon(Icons.favorite,
+                        size: 22,
+                        color: isFavo
+                            ? Colors.red
+                            : Colors.amber.withOpacity(0.4)))
+              ],
+            );
+          },
+        ),
       ),
     );
   }
