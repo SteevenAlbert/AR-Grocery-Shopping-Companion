3d6778a629bed18345c3cf60aedf62736e4be4f6
laila-elhattab <liko.hani@gmail.com>
2023-01-20 04:58:27 +0200

    fav_icon sizing

--- lib/presentation/home/components/fav_icon.dart
+++ lib/presentation/home/components/fav_icon.dart
@@ -27,73 +27,76 @@ class _FavIconState extends State<FavIcon> {
             return Column(
               children: [
                 Container(
+                  width: 37,
                   decoration: BoxDecoration(
                       color: darkMode == ThemeMode.light
                           ? Colors.amber.withOpacity(0.1)
                           : Theme.of(context).primaryColor.withOpacity(0.1),
                       shape: BoxShape.circle),
-                  child: IconButton(
-                      onPressed: () {
-                        if (isFavo) {
-                          isFavo = false;
-                        } else {
-                          isFavo = true;
-                        }
-                        if (isFavo) {
-                          ref
-                              .read(favsProvider.notifier)
-                              .addItem(widget.product.id);
-                        } else {
-                          ref
-                              .read(favsProvider.notifier)
-                              .removeItem(widget.product.id);
-                        }
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          margin: const EdgeInsets.only(
-                              left: 50, right: 50, bottom: 70),
-                          elevation: 6.0,
-                          backgroundColor: Theme.of(context).primaryColor,
-                          behavior: SnackBarBehavior.floating,
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                          duration: const Duration(seconds: 2),
-                          content: isFavo
-                              ? Row(
-                                  children: [
-                                    Icon(Icons.check_circle,
-                                        color: Colors.white),
-                                    SizedBox(width: 6),
-                                    Text(
-                                      'Item added to favorites',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          letterSpacing: 0.7),
-                                    ),
-                                  ],
-                                )
-                              : Row(
-                                  children: [
-                                    Icon(Icons.check_circle,
-                                        color: Colors.white),
-                                    SizedBox(width: 4),
-                                    Text(
-                                      'Item removed from favorites',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          letterSpacing: 0.3),
-                                    ),
-                                  ],
-                                ),
-                        ));
-                      },
-                      icon: isFavo
-                          ? Icon(Icons.favorite, size: 25, color: Colors.red)
-                          : Icon(Icons.favorite_border,
-                              size: 25,
-                              color: darkMode == ThemeMode.light
-                                  ? Colors.amber.withOpacity(0.4)
-                                  : Theme.of(context).primaryColor)),
+                  child: Center(
+                    child: IconButton(
+                        onPressed: () {
+                          if (isFavo) {
+                            isFavo = false;
+                          } else {
+                            isFavo = true;
+                          }
+                          if (isFavo) {
+                            ref
+                                .read(favsProvider.notifier)
+                                .addItem(widget.product.id);
+                          } else {
+                            ref
+                                .read(favsProvider.notifier)
+                                .removeItem(widget.product.id);
+                          }
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            margin: const EdgeInsets.only(
+                                left: 50, right: 50, bottom: 70),
+                            elevation: 6.0,
+                            backgroundColor: Theme.of(context).primaryColor,
+                            behavior: SnackBarBehavior.floating,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            duration: const Duration(seconds: 2),
+                            content: isFavo
+                                ? Row(
+                                    children: [
+                                      Icon(Icons.check_circle,
+                                          color: Colors.white),
+                                      SizedBox(width: 6),
+                                      Text(
+                                        'Item added to favorites',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            letterSpacing: 0.7),
+                                      ),
+                                    ],
+                                  )
+                                : Row(
+                                    children: [
+                                      Icon(Icons.check_circle,
+                                          color: Colors.white),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        'Item removed from favorites',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            letterSpacing: 0.3),
+                                      ),
+                                    ],
+                                  ),
+                          ));
+                        },
+                        icon: isFavo
+                            ? Icon(Icons.favorite, size: 23, color: Colors.red)
+                            : Icon(Icons.favorite_border,
+                                size: 23,
+                                color: darkMode == ThemeMode.light
+                                    ? Colors.amber.withOpacity(0.4)
+                                    : Theme.of(context).primaryColor)),
+                  ),
                 )
               ],
             );
